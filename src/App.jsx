d7c69e5226626c5630990a37cc320e0fcfd89070
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-26 01:09:57 +0500

    setup:create-vite-app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Layout,Typography,Space} from "antd"
+function App() {
+  return (
+    <>
+ <BrowserRouter>
+ <h1>Crypto App</h1>
+      {/* <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes> */}
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
