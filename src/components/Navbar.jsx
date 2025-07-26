39de4b05acdbe46df76c4f425e7743969eceede6
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-27 01:39:12 +0500

    component/NAvbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button,MEnu,Typography,Avatar}from 'antd';
+import {Link} from "react-router-dom";
+import { HomeOutlined,MoneyCollectOutlined,BulbOutlined,FunctionOutlined,MenuOutlined } from '@ant-design/icons';
+export const Navbar = () => {
+  return (
+    <>
+
+    </>
+  )
+}
